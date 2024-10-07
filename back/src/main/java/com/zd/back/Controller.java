1e6e4a31ce97eab232ac258a68348096b5c87098
Yoonjae <rurujeon@gmail.com>
2024-10-07 20:15:00 +0900

    first commit

---
+++ back/src/main/java/com/zd/back/Controller.java
@@ -0,0 +1,15 @@
+package com.zd.back;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    
+    @GetMapping("/")
+    @ResponseBody   //문자열 그대로 응답
+    public String index(){
+        return "왜 안나오냐";
+    }
+}
