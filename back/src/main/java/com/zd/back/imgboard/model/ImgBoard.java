5f2e10b225254e17b160fc88104d36a1c3645147
progseMOON <progse0406@gmail.com>
2024-10-10 19:18:35 +0900

    imgModel

--- back/src/main/java/com/zd/back/imgboard/model/ImgBoard.java
+++ back/src/main/java/com/zd/back/imgboard/model/ImgBoard.java
@@ -9,17 +9,17 @@ import lombok.Data;
 public class ImgBoard {
 
     private Long no;     
-    private String userid; 
+    private String userId; 
     private String cate;     
     private String title;     
     private String content; 
-    private String filepath; // 파일 경로 
+    private String filePath; // 파일 경로 
     private Timestamp created;    
     private int auth; // 인증  1 일때 인증 ,0 일때 미인증 
-    private String savefilename; // 저장된 파일명   
-    private String originalfilename; // 원본 파일명  
+    private String saveFilename; // 저장된 파일명   
+    private String originalFilename; // 원본 파일명  
     private int point;
-    private Timestamp authdate;    
+    private Timestamp authDate;    
     private String pwd;
    
 
