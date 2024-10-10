fd23464b5766c01df4ae56fe314d00f75e3b434b
Yoonjae <rurujeon@gmail.com>
2024-10-10 16:19:41 +0900

    기본틀

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MainPageApp from './components/mainPage/MainPageApp';
+import LoginApp from './components/login/LoginApp';
+
+function App() {
+  return (
+    <div>
+      <MainPageApp/>
+      {/* <LoginApp/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/mainPage/MainPageApp.js
@@ -0,0 +1,14 @@
+import Body from "./Body";
+import Header from "./Header";
+
+
+function MainPageApp() {
+    return (
+      <div>
+        <Header/>
+        <Body/>
+      </div>
+    );
+  }
+  
+  export default MainPageApp;
