5d18a7cdc9437485b964779de4973436bec0bcdb
Yoonjae <rurujeon@gmail.com>
2024-10-10 19:50:41 +0900

    10/10 커밋

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainPageApp from './components/mainPage/MainPageApp';
-import LoginApp from './components/login/LoginApp';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/mainPage/Header';
+import Footer from './components/mainPage/Footer';
 
 function App() {
   return (
-    <div>
-      {/* <MainPageApp/> */}
-      <LoginApp/>
-    </div>
+    <BrowserRouter>
+
+      <MainPageApp/>
+      {/* <LoginApp/> */}
+
+    </BrowserRouter>
   );
 }
 
