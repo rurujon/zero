d28db0aa73f95ebaaa6fe9b34209a895eec86c89
progseMOON <progse0406@gmail.com>
2024-10-10 19:17:40 +0900

    imgModel

--- back/src/main/java/com/zd/back/imgboard/model/ImgBoard.java
+++ back/src/main/java/com/zd/back/imgboard/model/ImgBoard.java
@@ -1,26 +1,27 @@
 package com.zd.back.imgboard.model;
 
+
 import java.sql.Timestamp;
+
 import lombok.Data;
 
 @Data
 public class ImgBoard {
-    
-    private int no;
-    private String userId;
-    private String cate;
-    private String title;
-    private String content;
-    private String filePath;
-    private Timestamp created;
-    private String auth ;
-    private String saveFileName;
-    private String originalFileName;
+
+    private Long no;     
+    private String userid; 
+    private String cate;     
+    private String title;     
+    private String content; 
+    private String filepath; // 파일 경로 
+    private Timestamp created;    
+    private int auth; // 인증  1 일때 인증 ,0 일때 미인증 
+    private String savefilename; // 저장된 파일명   
+    private String originalfilename; // 원본 파일명  
     private int point;
-    private Timestamp authDate;
+    private Timestamp authdate;    
     private String pwd;
-
-
+   
 
 
 }
