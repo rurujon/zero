a76b26af4925d00b4244c134641b3574679959c5
progseMOON <progse0406@gmail.com>
2024-10-14 20:08:59 +0900

    modified:   front/src/App.js 	modified:   front/src/components/imgboard/Imgcreated.js 	modified:   front/src/components/login/RegisterPage.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import MainPageApp from './components/mainPage/MainPageApp';
+import ImgApp from './components/imgboard/ImgApp';
 import { BrowserRouter } from 'react-router-dom';
+/* import MainPageApp from './components/mainPage/MainPageApp';
 import Header from './components/mainPage/Header';
 import Footer from './components/mainPage/Footer';
-import ImgApp from './components/imgboard/ImgApp';
 import LoginApp from './components/login/LoginApp';
 import NewsList from './components/naverapi/NewsList';
-
+ */
 function App() {
   return (
-    <BrowserRouter>
+      <BrowserRouter>
     <div>
   
        {/* <MainPageApp/>
@@ -20,9 +20,9 @@ function App() {
     </div>
       {/* <MainPageApp/> */}
       {/* <LoginApp/> */}
-      <NewsList/>
-
+    {/*   <NewsList/> */}
     </BrowserRouter>
+
   );
 }
 
