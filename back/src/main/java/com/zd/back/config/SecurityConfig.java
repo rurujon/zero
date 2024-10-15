29efd1bdde9bd838d60e2f5e14613e6781ceb00c
name <email>
2024-10-15 14:50:47 +0900

    24.10.15 정우준

--- back/src/main/java/com/zd/back/config/SecurityConfig.java
+++ back/src/main/java/com/zd/back/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.zd.back.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -12,22 +17,33 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/api/members/register", "/api/members/login","/api/imgboard/*").permitAll()
-            .anyRequest().authenticated()
-            //.antMatchers("/member/register", "/member/login", "/member/info").permitAll()
-            .anyRequest().permitAll()//authenticated()
-            .and()
-            .formLogin().disable();
-        return http.build();
+        http.cors()
+        .and()
+        .csrf().disable()
+        .authorizeHttpRequests()
+        .antMatchers("/member/register", "/member/login", "/member/info", "/member/**", "/member/logout").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin().disable();
+    return http.build();
     }
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 
--- back/src/main/java/com/zd/back/config/WebConfig.java
+++ back/src/main/java/com/zd/back/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.zd.back.login.config;
+package com.zd.back.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
