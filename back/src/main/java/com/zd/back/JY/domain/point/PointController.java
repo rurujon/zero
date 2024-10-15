f5f7b4f286d65ede326add0bdc4b828afabc27dc
조준영 <idwnsudd1965@naver.com>
2024-10-15 15:42:37 +0900

    준영 10-15/15:42

--- back/src/main/java/com/zd/back/JY/domain/point/PointController.java
+++ back/src/main/java/com/zd/back/JY/domain/point/PointController.java
@@ -39,17 +39,15 @@ public class PointController {
     public ModelAndView result_ok(PointDTO dto, HttpServletRequest request) throws Exception {
         ModelAndView mav = new ModelAndView();
 
-        dto.setPointid(pointService.maxNum()+1);
+        dto.setPointId(pointService.maxNum()+1);
 
         // pointService.insertData(dto);
         
-        System.out.print("\n\n POSTMappint \n\n");
 
-        dto = pointService.findById(2);
+        dto = pointService.findById(Integer.parseInt(request.getParameter("pointid")));
 
-        System.out.println("findById \n\n\n" +dto.getPointid() +"\n\n\n findById");
 
-        mav.addObject("id", dto.getPointid());
+        mav.addObject("pointId", dto.getPointId());
         mav.addObject("usedPoint", dto.getUsedPoint());
         mav.addObject("maxPoint", dto.getMaxPoint());
 
@@ -69,10 +67,10 @@ public class PointController {
     }
       
     @PostMapping("/manage.action")
-    public ModelAndView upDown(HttpServletRequest request) {
+    public ModelAndView upDown(HttpServletRequest request) throws Exception {
         ModelAndView mav = new ModelAndView();
         
-        int id = Integer.parseInt(request.getParameter("id"));
+        int pointId = Integer.parseInt(request.getParameter("pointId"));
 
         String oper = request.getParameter("oper");
         System.out.println("oper: "+oper);
@@ -81,23 +79,36 @@ public class PointController {
 
         System.out.println("updown: "+ updown);
 
-        PointDTO dto = pointService.findById(id);
+        PointDTO dto = pointService.findById(pointId);
+
+        System.out.println(dto.getMemId());
+        System.out.println(dto.getPointId());
+        System.out.println(dto.getMaxPoint());
+        System.out.println(dto.getUsedPoint());
+        System.out.println(dto.getGrade());
+
+        //dto.setGrade(dto.getGrade());
 
         if(oper.equals("+")){
             dto.setUsedPoint(dto.getUsedPoint()+updown);
 
             dto.setMaxPoint(dto.getMaxPoint()+updown);
 
-            pointService.updatePoint(dto);
 
         }else if(oper.equals("-")){
 
+            if(dto.getUsedPoint()-updown<0){
+                throw new Exception("0보다 작을 수 없습니다.");
+            }
             dto.setUsedPoint(dto.getUsedPoint()-updown);
+
         }
+
+        pointService.updatePoint(dto);
 
 
         
-        mav.addObject("id", dto.getPointid());
+        mav.addObject("pointId", dto.getPointId());
         mav.addObject("usedPoint", dto.getUsedPoint());
         mav.addObject("maxPoint", dto.getMaxPoint());
         mav.setViewName("/JY/result");
@@ -113,23 +124,34 @@ public class PointController {
 
 
         mav.setViewName("/JY/signup");
-
+        System.out.println("getMapping/signup.action 114line");
         return mav;
     }
 
     @PostMapping("/signup_ok")
-    public ModelAndView signPoint_ok() {
+    public ModelAndView signPoint_ok( HttpServletRequest request) {
         ModelAndView mav = new ModelAndView();
-
+        dto = new PointDTO();
+        
         int maxNum = pointService.maxNum();
         
-        dto = new PointDTO();
+        String memId = request.getParameter("memId");
 
-        dto.setPointid(maxNum+1);
+        System.out.println(maxNum);
+        System.out.println(memId);
 
+        dto.setPointId(maxNum+1);
+        dto.setMemId(memId);
+        dto.setGrade("level1");
+        
+        System.out.println("postMapping/signup.ok 130Line");
         pointService.insertData(dto);
 
-        System.out.println("등록 완료 id: "+dto.getPointid());
+        System.out.println("등록 완료 id: "+dto.getPointId());
+        System.out.println("maxPoint: "+ dto.getMaxPoint());
+        System.out.println("usedPoint: "+ dto.getUsedPoint());
+        System.out.println("memId: "+dto.getMemId());
+        System.out.println("grade: "+dto.getGrade());
 
         mav.setViewName("/JY/index");
 
@@ -148,13 +170,13 @@ public class PointController {
     @PostMapping("/find_ok")
     public ModelAndView postMethodName(HttpServletRequest request) {
 
-        int id = Integer.parseInt(request.getParameter("id"));
+        int pointId = Integer.parseInt(request.getParameter("pointId"));
 
         ModelAndView mav = new ModelAndView();
 
-        dto = pointService.findById(id);
+        dto = pointService.findById(pointId);
 
-        mav.addObject("id", dto.getPointid());
+        mav.addObject("pointId", dto.getPointId());
         mav.addObject("usedPoint", dto.getUsedPoint());
         mav.addObject("maxPoint",dto.getMaxPoint());
 
--- back/src/main/java/com/zd/back/JY/domain/point/PointDTO.java
+++ back/src/main/java/com/zd/back/JY/domain/point/PointDTO.java
@@ -2,12 +2,17 @@ package com.zd.back.JY.domain.point;
 
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class PointDTO {
-    private int pointid;
+    private int pointId;
     private int maxPoint;
     private int usedPoint;
     private String memId;
     private String grade;
 }
+
+
