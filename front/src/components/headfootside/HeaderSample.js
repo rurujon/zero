307776fe8c0d4abf4ee7a8af1e4c76635615e951
Yoonjae <rurujeon@gmail.com>
2024-11-01 19:08:45 +0900

    헤더 아이콘 몇개 추가

--- front/src/components/headfootside/HeaderSample.js
+++ front/src/components/headfootside/HeaderSample.js
@@ -5,6 +5,7 @@ import './HeaderSample.css';
 import { Link, Navigate } from 'react-router-dom';
 
 
+
 const HeaderSample = () => {
 
     const [isQuizModalOpen, setIsQuizModalOpen] = useState(false); // 모달 상태 추가
@@ -110,8 +111,8 @@ const HeaderSample = () => {
                             
                         </nav>
                         <div>
-                            
-                            <div>로그</div>
+                            <img src='/images/home.png'></img>
+                            <img src='/images/login.png'></img>
                         </div>
                     </div>
                     {activeMenu && (
