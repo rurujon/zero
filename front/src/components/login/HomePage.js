a25ae5ef66420a1b013fd412eeb393b80c0bbfc3
name <email>
2024-11-05 15:19:04 +0900

    우준--퀴즈버튼추가

--- front/src/components/login/HomePage.js
+++ front/src/components/login/HomePage.js
@@ -102,8 +102,9 @@ const HomePage = () => {
                     <h4>오늘도 행복한 하루되세요.</h4>
 
                     <p>역할: {role}</p>
-                    <button onClick={handleMemberInfo} className="btn btn-info">My정보조회</button>&nbsp;
+                    <button onClick={handleMemberInfo} className="btn btn-info">MyInfo조회</button>&nbsp;
                     <button onClick={handlePointInfo} className="btn btn-success">POINT조회</button>&nbsp;
+                    <button onClick={''} className="btn btn-primary">일일QUIZ</button>&nbsp;
                     <button onClick={handleLogout} className="btn btn-danger">로그아웃</button>&nbsp;
                     {role === 'ADMIN' && (
                         <button onClick={() => navigate('/admin')} className="btn btn-warning">관리자 페이지</button>
