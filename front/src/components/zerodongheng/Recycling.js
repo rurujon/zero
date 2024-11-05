099301353c4b2f5cb89102bd940a023015ffda3b
조준영 <idwnsudd1965@naver.com>
2024-11-05 18:45:49 +0900

    11-05/18:45 / recycle임시

--- front/src/components/zerodongheng/Recycling.js
+++ front/src/components/zerodongheng/Recycling.js
@@ -5,28 +5,35 @@ const Recycling = () => {
         <div className="zero-waste-intro">
         <h2>리사이클링이란?</h2>
         <p>
-          리사이클링은 사용된 물건을 새로운 제품으로 만드는 과정으로, 자원의 낭비를 줄이고 환경을 보호하는 중요한 방법입니다. 
+          리사이클링은 사용된 물건을 새로운 제품으로 만드는 과정으로, 자원의 낭비를 줄이고 환경을 보호하는 중요한 방법입니다. <br/>
           많은 자원을 절약할 수 있으며, 쓰레기를 줄이는 데 기여할 수 있습니다.
         </p>
 
         <br/><br/>
         <hr/>
         <br/><br/>
-
-        <h3 className='right-align'>리사이클링의 필요성</h3>
-        <ul className='right-align'>
-          <li><strong>자원 절약:</strong> 천연 자원의 소모를 줄이고 보호할 수 있습니다.</li>
-          <li><strong>환경 보호:</strong> 매립지와 소각장에서 발생하는 환경 오염을 감소시킵니다.</li>
-          <li><strong>에너지 절약:</strong> 신제품을 생산하는 데 드는 에너지를 줄여줍니다.</li>
-        </ul>
-
+        <div style={{display:'flex', justifyContent:'center', flexDirection:'row'}}>
+          <div style={{width:'45%'}}>
+            <img src='/images/zerodonghaeng/recyclepet.jpg' alt='recycle'
+            style={{height:'400px'}}/>
+          </div>
+          <div style={{width:'45%'}}>
+            <h3 className='right-align'>리사이클링의 필요성</h3>
+            <ul className='right-align'>
+              <li><strong>자원 절약:</strong> 천연 자원의 소모를 줄이고 보호할 수 있습니다.</li>
+              <li><strong>환경 보호:</strong> 매립지와 소각장에서 발생하는 환경 오염을 감소시킵니다.</li>
+              <li><strong>에너지 절약:</strong> 신제품을 생산하는 데 드는 에너지를 줄여줍니다.</li>
+            </ul>
+          </div>
+        </div>
         <br/><br/>
         <hr/>
         <br/><br/>
 
         <h3>리사이클링 방법</h3>
         <p>
-          리사이클링을 실천하기 위해 가정에서 분리 배출을 철저히 하고, 재활용이 가능한 자원을 분류하여 
+          리사이클링을 실천하기 위해 가정에서 분리 배출을 철저히 하고, <br/>
+          재활용이 가능한 자원을 분류하여 
           올바르게 버리는 습관을 들이는 것이 중요합니다.
         </p>
       </div>
