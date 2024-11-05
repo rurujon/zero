ac0065fd0423bc0846d1b755e99504357fc5f789
Yoonjae <rurujeon@gmail.com>
2024-11-05 16:24:57 +0900

    스와이퍼 라이브러리 다운 필요

--- front/src/components/mainPage/BannerSlider.js
+++ front/src/components/mainPage/BannerSlider.js
@@ -1,56 +1,36 @@
-// BannerSlider.js
-import React, { useState, useEffect } from 'react';
-import './BannerSlider.css';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+
+import './BannerSlider.css'; // 추가적인 스타일이 필요한 경우 사용
 
 const BannerSlider = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
   const images = [
     '/images/banner.jpeg',
-    '/images/logo.png' // 다른 이미지 추가
+    '/images/logo.png',
+    '/images/HeaderImage.jpg'
   ];
 
-  useEffect(() => {
-    const slideInterval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000);
-
-    return () => clearInterval(slideInterval);
-  }, [3000, images.length]);
-
-  const goToPrevious = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-  };
-
-  const goToNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
   return (
-    <div className="banner-slider">
-      <div className="slides" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className={`slide ${index === currentIndex ? 'active' : ''}`}
-            style={{ backgroundImage: `url(${image})` }}
-          />
-        ))}
-      </div>
-
-      <button className="prev" onClick={goToPrevious}>❮</button>
-      <button className="next" onClick={goToNext}>❯</button>
-
-      <div className="indicators">
-        {images.map((_, index) => (
-          <button
-            key={index}
-            className={`indicator ${index === currentIndex ? 'active' : ''}`}
-            onClick={() => setCurrentIndex(index)}
-          />
-        ))}
-      </div>
-    </div>
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay]}
+      navigation
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 3000 }}
+      loop={true}
+      className="banner-slider"
+    >
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <div className="slide">
+            <img src={image} alt={`Slide ${index}`} className="slide-image" />
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
