974637f7883144e212b1e9c8af10dbfb37252840
name <email>
2024-11-11 06:54:46 +0900

    우준--토큰관리인증개선

--- front/src/components/login/utils/AxiosInterceptor.js
+++ front/src/components/login/utils/AxiosInterceptor.js
@@ -1,34 +1,36 @@
 import React, { useContext, useEffect } from 'react';
 import axios from 'axios';
 import { AuthContext } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
-const AxiosInterceptor = ({ children, navigate }) => {
-    const { refreshAccessToken, logout } = useContext(AuthContext);
+const AxiosInterceptor = ({ children }) => {
+  const { refreshAccessToken, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const interceptor = axios.interceptors.response.use(
-            (response) => response,
-            async (error) => {
-                if (error.response && error.response.status === 403) {
-                    const newToken = await refreshAccessToken();
-                    if (newToken) {
-                        error.config.headers['Authorization'] = `Bearer ${newToken}`;
-                        return axios.request(error.config);
-                    } else {
-                        logout();
-                        navigate('/login');
-                    }
-                }
-                return Promise.reject(error);
-            }
-        );
+  useEffect(() => {
+    const interceptor = axios.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (error.response && error.response.status === 401) {
+          const newToken = await refreshAccessToken();
+          if (newToken) {
+            error.config.headers['Authorization'] = `Bearer ${newToken}`;
+            return axios.request(error.config);
+          } else {
+            logout();
+            navigate('/login', { state: { message: '세션이 만료되었습니다. 다시 로그인해주세요.' } });
+          }
+        }
+        return Promise.reject(error);
+      }
+    );
 
-        return () => {
-            axios.interceptors.response.eject(interceptor);
-        };
-    }, [refreshAccessToken, logout, navigate]);
+    return () => {
+      axios.interceptors.response.eject(interceptor);
+    };
+  }, [refreshAccessToken, logout, navigate]);
 
-    return <>{children}</>;
+  return <>{children}</>;
 };
 
 export default AxiosInterceptor;
