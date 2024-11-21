ecba0a766bf3d3112478cfc8396dc85630cbb985
progseMOON <progse0406@gmail.com>
2024-11-21 14:42:29 +0900

    승은-- redis 백업

---
+++ back/src/main/java/com/zd/back/login/security/JwtFilter copy.java
@@ -0,0 +1,58 @@
+package com.zd.back.login.security;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/* @Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final TokenProvider tokenProvider;
+    private final Template<String, String> Template;
+
+    public JwtFilter(TokenProvider tokenProvider, Template<String, String> Template) {
+        this.tokenProvider = tokenProvider;
+        this.Template = Template;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String jwt = resolveToken(request);
+
+        if (jwt != null && tokenProvider.validateToken(jwt)) {
+            // Check if the token is in the blacklist
+            String isLogout = Template.opsForValue().get("blacklist:" + jwt);
+            if (ObjectUtils.isEmpty(isLogout)) {
+                Authentication authentication = tokenProvider.getAuthentication(jwt);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                // If the token is blacklisted, clear the security context
+                SecurityContextHolder.clearContext();
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("{\"error\": \"This token has been logged out.\"}");
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
+ */
